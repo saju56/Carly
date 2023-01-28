f318e27c68edcae75d895742cd5629b8bfed0b0d
simonsrz <szymonslusarz01@gmail.com>
2023-01-28 12:43:01 +0100

    views created

---
+++ mobile/views/LoginView.tsx
@@ -0,0 +1,26 @@
+import { StatusBar } from "expo-status-bar";
+import { useContext } from "react";
+import { StyleSheet, Text, View, Button, Image } from "react-native";
+
+
+export default function LoginView() {
+  return (
+    <View style={styles.container}>
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
+
