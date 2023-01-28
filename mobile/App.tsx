e0c1d349d2c38bbfef96acbb4154f5b1a1fbc904
simonsrz <szymonslusarz01@gmail.com>
2023-01-28 15:51:33 +0100

    base navigation

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,22 +1,33 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import LoginView from "./views/LoginView";
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import AdminMenuView from "./views/AdminMenuView";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text></Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+    headerShown: false
+  }}>
+        <Stack.Screen
+          name="Login"
+          component={LoginView}
+        />
+        <Stack.Screen name="AdminMenu" component={AdminMenuView} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
-
